7560a61512d3ae32602e8b71791b382589f706ea
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2021-10-03 14:06:08 +1300

    docs - Comments added; fix - Changed security level of fields, and description usage changed.
    Comments added.
    Changed security of fields and changed description use.

--- Desserts/app/src/main/java/com/example/desserts/structures/Edible.java
+++ Desserts/app/src/main/java/com/example/desserts/structures/Edible.java
@@ -4,66 +4,151 @@ import android.media.Image;
 
 import java.util.List;
 
+/**
+ * The Edible abstract class implements the Dessert Interface and is used to have the common
+ * features for edible dessert items.
+ * @author Osama Kashif
+ */
 abstract class Edible implements Dessert {
 
-    String name;
-    long Id;
-    float cost;
-    List<Image> images;
-    String description;
-    List<String> ingredientsContained;
-    List<String> dietsSuitableFor;
-    long numberViewed;
-    int peopleServed;
+    private final String name;
+    private final long Id;
+    private final float cost;
+    private final List<Image> images;
+    private final String basicDescription;
+    private String description = null;
+    private final List<String> ingredientsContained;
+    private final List<String> dietsSuitableFor;
+    private long numberViewed;
+    private final int peopleServed;
 
-    public Edible (String name, long Id, float cost, List<Image> images, String description, List<String> ingredientsContained, List<String> dietsSuitableFor, long numberViewed, int peopleServed) {
+    /**
+     * Constructor for Edible Dessert.
+     * @param name - name of Edible Dessert
+     * @param Id - ID of Edible Dessert
+     * @param cost - Cost of Edible Dessert
+     * @param images - Images of Edible Dessert
+     * @param basicDescription - Basic Description of Edible Dessert
+     * @param ingredientsContained - Ingredients Contained in the Edible Dessert
+     * @param dietsSuitableFor - Diets the Edible Dessert is Suitable For
+     * @param numberViewed - Number of times the Edible Dessert has been viewed
+     * @param peopleServed - People served by 1 serving of the Edible Dessert
+     */
+    public Edible (String name, long Id, float cost, List<Image> images, String basicDescription, List<String> ingredientsContained, List<String> dietsSuitableFor, long numberViewed, int peopleServed) {
         this.name = name;
         this.Id = Id;
         this.cost = cost;
         this.images = images;
-        this.description = description;
+        this.basicDescription = basicDescription;
         this.ingredientsContained = ingredientsContained;
         this.dietsSuitableFor = dietsSuitableFor;
         this.numberViewed = numberViewed;
         this.peopleServed = peopleServed;
     }
 
+    /**
+     * Gets the name of the Dessert.
+     * @return String name of Dessert
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the ID of the Edible Dessert.
+     * @return long ID.
+     */
     public long getId() {
         return Id;
     }
 
+    /**
+     * Gets the cost of the Dessert.
+     * @return float cost.
+     */
     public float getCost() {
         return cost;
     }
 
+    /**
+     * Gets all the images of the Edible Dessert.
+     * @return List of Images of Edible Dessert.
+     */
     public List<Image> getImages() {
         return images;
     }
 
+    /**
+     * Gets the particular image of the Edible Dessert.
+     * @param imageId - Number of the image wanted from all the images
+     * @return Particular Image of Edible Dessert
+     */
     public Image getImage(int imageId) {
         return images.get(imageId);
     }
 
+    /**
+     * Gets the description of the Edible Dessert.
+     * @return String description
+     */
     public String getDescription() {
-        return description;
+        if (description == null) {
+            return basicDescription;
+        } else {
+            return description;
+        }
     }
 
+    /**
+     *  Gets basic description of the Edible Dessert.
+     * @return String basic Description of Edible Dessert.
+     */
+    public String getBasicDescription() {
+        return basicDescription;
+    }
+
+    /**
+     * Used to edit the full description of the Edible Dessert.
+     * @param description - To enter the full custom description of the Edible Dessert.
+     */
+    public void addDescription (String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets some of the ingredients contained in the Edible Dessert for allergy reasons.
+     * @return List of Ingredients Contained
+     */
     public List<String> getContainedIngredients() {
         return ingredientsContained;
     }
 
+    /**
+     * Gets the diets the Edible Dessert is suitable for.
+     * @return List of Diets the Edible Dessert is suitable for
+     */
     public List<String> getDietsSuitableFor() {
         return dietsSuitableFor;
     }
 
+    /**
+     * Gets the number of times the Edible Dessert has been viewed.
+     * @return long number viewed.
+     */
     public long getNumberViewed() {
         return numberViewed;
     }
 
+    /**
+     * Increases the number of people who viewed the app at runtime.
+     */
+    public void increaseNumberViewed() {numberViewed++;}
+
+    /**
+     * Gets the people served by the Edible Dessert.
+     * Specific for  Edible Desserts.
+     * @return Number of people served by one serving of the Edible Dessert
+     */
     public String getAdditionalDetails() {
         return ("People served: " + peopleServed);
     }
