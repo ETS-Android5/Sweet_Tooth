ed73e1e08ed52a0c7a3eb02c2cf71925ae65f87f
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2021-10-03 14:03:57 +1300

    docs - Added comment; fix - Removed unnecessary method.
    Comments added.
    Unnecessary method removed.

--- Desserts/app/src/main/java/com/example/desserts/structures/Dessert.java
+++ Desserts/app/src/main/java/com/example/desserts/structures/Dessert.java
@@ -4,26 +4,71 @@ import android.media.Image;
 
 import java.util.List;
 
+/**
+ * The Dessert interface is to be used for the dessert items throughout the application and the
+ * dependency injection. It mostly has getters for features common for each dessert.
+ * @author Osama Kashif
+ */
 public interface Dessert {
 
+    /**
+     * Gets the name of the Dessert.
+     * @return String name of Dessert
+     */
     public String getName();
 
+    /**
+     * Gets the ID of the Dessert.
+     * @return long ID.
+     */
     public long getId();
 
+    /**
+     * Gets the cost of the Dessert.
+     * @return float cost.
+     */
     public float getCost();
 
+    /**
+     * Gets all the images of the Dessert.
+     * @return List of Images of Dessert.
+     */
     public List<Image> getImages();
 
+    /**
+     * Gets the particular image of the Dessert.
+     * @param imageId - Number of the image wanted from all the images
+     * @return Particular Image of Dessert
+     */
     public Image getImage(int imageId);
 
+    /**
+     * Gets the description of the Dessert.
+     * @return String description
+     */
     public String getDescription();
 
+    /**
+     * Gets some of the ingredients contained in the Dessert for allergy reasons.
+     * @return List of Ingredients Contained
+     */
     public List<String> getContainedIngredients();
 
+    /**
+     * Gets the diets the Dessert is suitable for.
+     * @return List of Diets the Dessert is suitable for
+     */
     public List<String> getDietsSuitableFor();
 
+    /**
+     * Gets the number of times the Dessert has been viewed.
+     * @return long number viewed.
+     */
     public long getNumberViewed();
 
-    public String getAdditionalDetails();
+    /**
+     * Increases the number of people who viewed the app at runtime.
+     */
+    public void increaseNumberViewed();
 
 }
